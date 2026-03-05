bcfbd2d3b3bb6ed2c4ab7d6082c3bd831717b621
Ramon Barreto Gomes <ramonbarreto1007@gmail.com>
2026-03-04 22:18:34 -0300

    Add files via upload

--- Pages/Login/script.js
+++ Pages/Login/script.js
@@ -1,9 +1,11 @@
 const inputLogin = document.getElementById("input-login")
-
 const inputSenha = document.getElementById("input-senha")
-
-// console.log(inputLogin);
+// ERROS
+// const msgErroLogin = document.getElementById("intput-erroemail");
+// const msgErrosenha = document.getElementById("intput-errosenha");
+// // console.log(inputLogin);
 // console.log(inputSenha);
+const msgErroLogin = document.getElementById("intput-errosenha");
 
 function login(eventrec){
     event.preventDefault(eventrec);
@@ -14,13 +16,37 @@ function login(eventrec){
     const SENHA_VALIDO = "1234";
 
     if (valorLogin == LOGIN_VALIDO && valorSenha == SENHA_VALIDO) {
+        rememberSave();
       window.location.href="../Listagem/index.html"
      
     }else{
-       alert("Login invalido")
-       
-        
+        msgErroLogin.style.display = 'block'
+        inputLogin.style.border ="1px solid red"
+        inputSenha.style.border ="1px solid red"
     }
+}
 
-    
+    const checkboxRemember = document.querySelector('input[type=checkbox]');
+
+const rememberSave = () => {
+    const valorLogin = inputLogin.value;
+    const valorSenha = inputSenha.value;
+
+    const dadosLogin ={
+        login:valorLogin,
+        senha:valorSenha
+    }
+  if (checkboxRemember.checked) {
+    localStorage.setItem('dadosLogin', JSON.stringify(dadosLogin))
+  }
+
+//  localStorage.setItem('dadosLogin', JSON.stringify(dadosLogin))
+
+}
+const ValorSalvo = JSON.parse(localStorage.getItem('dadosLogin'));
+
+
+if(ValorSalvo.login && ValorSalvo.senha){
+inputLogin.value = ValorSalvo.login;
+inputSenha.value = ValorSalvo.senha;
 }
